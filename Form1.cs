bd7e30fee7de4f0445bbed925a0202d5ef00f890
Jenny Chen <whober0521@gmail.com>
2018-12-09 16:17:58 -0600

    Separation

--- Form1.cs
+++ Form1.cs
@@ -17,7 +17,7 @@ namespace NonogramSolver
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            problem = new Problem("Rocket");
+            problem = new Problem("President");
 
             board = new Board(problem.ColumnHint.Length, problem.RowHint.Length);
             board.Location = new Point(110, 110);
--- Problem.cs
+++ Problem.cs
@@ -58,7 +58,7 @@ namespace NonogramSolver
                     ColumnHint = new int[][] {
                         new int[] { 5, 2, 9 }, new int[] {18, 2 }, new int[] { 19, 3 },new int[] { 22, 3, 1 },new int[] { 10, 2, 10, 1 },
                         new int[] { 8, 6 }, new int[] { 7, 8 }, new int[] { 6, 5, 8, 2 },new int[] { 6, 4, 8, 3 },new int[] { 5, 5, 12 },
-                        new int[] { 5, 4, 1, 1, 1, 9 }, new int[] {6, 6, 1, 4, 8}, new int[] { 6, 6, 3, 2, 8 },new int[] {6, 6, 3, 1, 7 },new int[] { 6, 5, 2, 1, 1, 7},
+                        new int[] { 5, 4, 1, 1, 1, 9 }, new int[] {6, 6, 1, 4, 8}, new int[] { 6, 8, 3, 2, 8 },new int[] {6, 6, 3, 1, 7 },new int[] { 6, 5, 2, 1, 1, 7},
                         new int[] { 6, 2, 1, 1, 1, 9 }, new int[] { 6, 2, 1, 1, 6 }, new int[] { 6, 3, 1, 1, 6 },new int[] { 6, 1, 2, 1, 1, 7 },new int[] { 5, 6, 6, 1, 1, 7 },
                         new int[] { 5, 5, 9, 1, 1, 7 }, new int[] { 4, 4, 1, 3, 3, 3, 7 }, new int[] { 3, 4, 1, 1, 2, 2, 7 },new int[] { 3, 5, 1, 1, 2, 1, 9 },new int[] { 4, 7, 1, 2, 1, 8},
                         new int[] { 4, 6, 15 }, new int[] { 5, 5, 3, 9 }, new int[] { 7, 5, 3, 11 },new int[] { 7, 3, 4, 14, 1 },new int[] { 5, 7, 2, 15, 4},
@@ -71,9 +71,9 @@ namespace NonogramSolver
                         new int[] { 4, 5 }, new int[] { 4, 6 }, new int[] { 4, 5 },new int[] { 4, 5 },new int[] { 4, 6 },
                         new int[] { 4, 5 }, new int[] { 4, 2, 3, 5  }, new int[] { 4, 6, 8 ,5 },new int[] { 4, 9, 10, 5},new int[] { 4, 9, 16 },
                         new int[] { 5, 8, 15 }, new int[] { 5, 3, 5, 2, 5, 4 }, new int[] { 2, 2, 1, 2, 1, 3, 3 },new int[] { 1, 1, 3, 3, 3 },new int[] { 1, 2, 1, 3, 3 },
-                        new int[] { 1, 2, 2, 3, 2 }, new int[] { 1, 3, 2, 3, 1 }, new int[] { 2, 1, 2, 1, 1, 4 },new int[] { 6, 1, 4 },new int[] { 4, 20 },
-                        new int[] {4, 2, 2, 6 }, new int[] { 6, 2, 1, 1, 3, 2,  4 }, new int[] { 3,2,6,2,1 },new int[] { 3,1,2,2 },new int[] { 2,2,2},
-                        new int[] { 5,2,9,2,2 }, new int[] { 4, 8, 1, 6  }, new int[] { 6, 1, 7 },new int[] { 8, 12 },new int[] { 22 },
+                        new int[] { 1, 2, 2, 3, 2 }, new int[] { 1, 3, 2, 3, 1 }, new int[] { 1, 2, 1, 3, 5, 1 },new int[] { 2,3,2,2,1,5,2 },new int[] { 2, 2, 1, 9, 7 },
+                        new int[] { 1, 3, 2, 6, 4, 3}, new int[] { 6, 2, 1, 1, 3, 2,  4 }, new int[] { 2, 1, 2, 1, 1, 4 },new int[] { 6, 1, 4 },new int[] { 4, 20 },
+                        new int[] { 4, 2, 2, 6 }, new int[] { 4, 8, 1, 6  }, new int[] { 6, 1, 7 },new int[] { 8, 12 },new int[] { 22 },
                         new int[] { 21 }, new int[] {19, 1 }, new int[] { 19, 2 },new int[] { 2, 16, 3 },new int[] { 2, 16, 5 }
                     };
                     break;
@@ -576,7 +576,11 @@ namespace NonogramSolver
 
                         if (black != 1 && empty != 0 && startidx >= hints[0] && result[idx - 1] == null && result[idx - 2] == true && black + 1 > hints[0])
                         {
-                            if (black + 1 > hints[1])
+                            if (black - 1 == hints[0])
+                            {
+                                idx = line.Length;
+                            }
+                            else if (black - 1 > hints[0] || black + 1 > hints[1])
                             {
                                 result[idx - 1] = false;
                             }
